26afc644f6659155ba9849989f7725c0d85dd421
bong <chaebonggi@naver.com>
2025-05-16 02:03:02 +0900

    .

--- baby-boomer/js/run.js
+++ baby-boomer/js/run.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
     // 1차 메뉴에 마우스 hover 시 서브 메뉴 표시
     $('#gnb .gnb-list > li').on('mouseenter', function() {
+        // #header에 open 클래스가 있으면 동작하지 않음
+        if ($('#header').hasClass('open')) return;
+
         var submenu = $(this).find('.gnb-2depth');
         if (submenu.length) {
             submenu.css('opacity', '1');
@@ -10,6 +13,8 @@ $(document).ready(function() {
 
     // 1차 메뉴에서 마우스가 벗어날 때 서브 메뉴 숨기기
     $('#gnb .gnb-list > li').on('mouseleave', function() {
+        if ($('#header').hasClass('open')) return;
+
         var submenu = $(this).find('.gnb-2depth');
         if (submenu.length) {
             submenu.css('opacity', '0');
@@ -19,6 +24,8 @@ $(document).ready(function() {
 
     // 서브 메뉴 항목에 포커스가 있을 때 서브 메뉴 표시
     $('#gnb .gnb-1depth > a').on('focusin', function() {
+        if ($('#header').hasClass('open')) return;
+
         var submenu = $(this).closest('li').find('.gnb-2depth');
         if (submenu.length) {
             submenu.css('opacity', '1');
@@ -28,6 +35,8 @@ $(document).ready(function() {
 
     // 서브 메뉴 항목에서 포커스를 벗어날 때 서브 메뉴 숨기기
     $('#gnb .gnb-1depth > a').on('focusout', function() {
+        if ($('#header').hasClass('open')) return;
+
         var submenu = $(this).closest('li').find('.gnb-2depth');
         setTimeout(function() {
             if (!submenu.find('a').is(':focus')) {
@@ -37,9 +46,20 @@ $(document).ready(function() {
         }, 100);
     });
 
+
+    $('#mobileMenuBtn').on('click', function (e) {
+        e.stopPropagation();
+
+        const $header = $('#header');
+        $header.toggleClass('open');
+
+        $('body').toggleClass('scroll-hide', $header.hasClass('open'));
+        $('#topBtn').toggleClass('hide', $header.hasClass('open'));
+    });
+
     const swiper = new Swiper(".mySwiper", {
-        slidesPerView: 3,
-        spaceBetween: 20,
+        slidesPerView: 2,
+        spaceBetween: 4,
         loop: true,
         autoplay: {
             delay: 3000,
@@ -49,13 +69,13 @@ $(document).ready(function() {
             nextEl: ".slide-wrap .swiper-button-next",
             prevEl: ".slide-wrap .swiper-button-prev"
         },
-        // breakpoints: {
-        //     768: {
-        //         slidesPerView: 3,
-        //         spaceBetween: 20,
-        //         centeredSlides: true,
-        //     }
-        // }
+        breakpoints: {
+            768: {
+                slidesPerView: 3,
+                spaceBetween: 20,
+                // centeredSlides: true,
+            }
+        }
     });
     $(".slide-wrap .swiper-start").hide();
     $(".slide-wrap .swiper-pause").on("click", function(){
@@ -75,8 +95,8 @@ $(document).ready(function() {
             $("#header").removeClass("fixed");
         }
     });
-
+    AOS.init();
 });
-// $(function(){
-//     $("html, body").animate({ scrollTop: 0 }, "fast"); 
-// });
+$(function(){
+    $("html, body").animate({ scrollTop: 0 }, "fast"); 
+});
